d4dbbbf4e5e2d63128f1350d599d5094207bdd14
Addison <addisondrewmoore@gmail.com>
2020-02-29 17:19:02 -0800

    added user controller and removed verify routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   user: require("./userRoutes"),
-  verify: require("./verifyRoutes"),
   gallery: require("./galleryRoutes"),
   photo: require("./photoRoutes"),
   news: require("./newsRoutes")
