688b3c9003180d6900c4f5c677cbac832e08fcd6
Addison Moore <addisondrewmoore@gmail.com>
2020-06-07 05:46:19 -0700

    delete working

--- controllers/photo.js
+++ controllers/photo.js
@@ -1,4 +1,5 @@
-const Photo = require("../Models/Photo");
+const db = require("../Models"),
+  mongodb = require("mongodb");
 
 module.exports = {
   search: (req, res) => {
@@ -8,7 +9,7 @@ module.exports = {
     } else {
       let search = new RegExp(req.body.query, "i");
       //   console.log(search);
-      Photo.find(
+      db.Photo.find(
         { $or: [{ caption: search }, { gallery: search }] },
         (err, photos) => {
           if (err) {
@@ -19,5 +20,71 @@ module.exports = {
         }
       );
     }
-  }
+  },
+  delete: (req, res) => {
+    console.log(`deleting photo: ${req.params.id}`);
+    db.Photo.findOne({ _id: mongodb.ObjectId(req.params.id) }, (err, photo) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ err });
+      } else {
+        console.log(photo);
+        if (photo) {
+          db.Gallery.update(
+            { name: photo.gallery, category: photo.category },
+            {
+              $pull: { photos: { _id: mongodb.ObjectId(req.params.id) } },
+            },
+            (err, gallery) => {
+              if (err) {
+                console.log(err);
+                return res.status(500).json({ err });
+              } else if (gallery) {
+                console.log(gallery);
+              }
+            }
+          );
+        }
+        res.status(200).json({ message: "Photo successfully deleted" });
+      }
+    });
+  },
+
+  // delete: (req, res) => {
+  //   console.log(`deleting photo: ${req.params.id}`);
+  //   db.Gallery.update(
+  //     {},
+  //     {
+  //       $pull: {
+  //         photos: { _id: mongodb.ObjectId(req.params.id) },
+  //       },
+  //     },
+  //     (err, photo) => {
+  //       if (err) {
+  //         console.log(err);
+  //         return res.status(500).json({ err });
+  //       } else {
+  //         console.log(photo);
+  //       }
+  //     }
+  //   );
+  // },
+
+  // delete: (req, res) => {
+  //   console.log(`deleting photo: ${req.params.id}`);
+  //   db.Gallery.findById(req.params.id, (err, photo) => {
+  //     if (err) {
+  //       console.log(err);
+  //       return res.status(500).json({ err });
+  //     } else {
+  //       console.log(photo);
+  //       res.status(200).json({ message: "Photo successfully deleted" });
+  //     }
+  //   });
+  // },
 };
+
+// db.getCollection("galleries").update(
+//   { name: "Welcome Cocco" },
+//   { $pull: { photos: { _id: ObjectId("5edcbb8830e525e0bc906f16") } } }
+// );
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -1,12 +1,35 @@
 const express = require("express"),
   router = express.Router(),
+  jwt = require("jsonwebtoken"),
+  config = require("../config/config"),
   controllers = require("../controllers");
 
 module.exports = router
   .post("/search", controllers.photo.search)
   // .get("/c/:category", controllers.photo.byCategory)
   // .get("/:name", controllers.photo.byName)
-  .get("/test", function(req, res) {
-    res.json({ message: "test successful" });
+  .delete("/:id", verifyToken, controllers.photo.delete)
+  .get("/test/:id", verifyToken, function (req, res) {
+    console.log(req.params.id);
+    res.json(req.params);
     console.log("test successful");
   });
+
+function verifyToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  console.log("triggered token check", bearerHeader);
+
+  if (typeof bearerHeader !== "undefined") {
+    console.log("checking token");
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    let verified = jwt.verify(req.token, config.jwtSecret);
+    console.log("here is the verified", verified);
+    req.userId = verified._id; //set user id for routes to use
+    next();
+  } else {
+    console.log("token check failed");
+    res.sendStatus(403);
+  }
+}
