e058a8b01a4ea965b297be4df95add414f4b70ca
Addison Moore <addisondrewmoore@gmail.com>
2020-07-23 18:14:14 -0700

    resize uploaded photo thumbnails

--- controllers/photo.js
+++ controllers/photo.js
@@ -4,36 +4,24 @@ const db = require("../Models"),
   Jimp = require("jimp"),
   fs = require("fs");
 
-// Check File Type
-function checkFileType(file, cb) {
-  // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
-  // Check ext
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  // Check mime
-  const mimetype = filetypes.test(file.mimetype);
-
-  if (mimetype && extname) {
-    return cb(null, true);
-  } else {
-    cb("Error: Images Only!");
+function errCheck(err) {
+  if (err) {
+    console.log(err);
+    return res.status(500).json({ err });
   }
 }
 
 module.exports = {
   search: (req, res) => {
-    // console.log(req.body);
     if (!req.body || !req.body.query) {
       res.status(404).json("Invalid: Missing query string");
     } else {
       let search = new RegExp(req.body.query, "i");
-      //   console.log(search);
       db.Gallery.find(
         {
           $or: [{ "photos.caption": search }, { "photos.searchTags": search }],
         },
         (err, galleries) => {
-          // db.Gallery.find({ "photos.caption": search }, (err, galleries) => {
           if (err) {
             res.status(404).json("unable to get photos");
             return console.log(err);
@@ -64,51 +52,39 @@ module.exports = {
     db.Gallery.findOne(
       { "photos._id": mongodb.ObjectId(req.params.id) },
       (err, gallery) => {
-        if (err) {
-          console.log(err);
-          return res.status(500).json({ err });
-        } else {
-          console.log(gallery);
-          if (gallery && location) {
-            try {
-              fs.unlinkSync(
-                `${__dirname}/../uploads/photos/${
-                  gallery.category.toLowerCase() == "advertising"
-                    ? "Client-Work"
-                    : gallery.category.replace(/\/?\s+/g, "_")
-                }/${gallery.name.replace(/\/?\s+/g, "-")}/${location}`
-              );
-              fs.unlinkSync(
-                `${__dirname}/../uploads/photos/${
-                  gallery.category.toLowerCase() == "advertising"
-                    ? "Client-Work"
-                    : gallery.category.replace(/\/?\s+/g, "_")
-                }/${gallery.name
-                  .replace(/\/?\s+/g, "_")
-                  .replace(/[^\w\s]/gi, "")}/thumbs/${location}`
-              );
-            } catch (err) {
-              console.log(err);
-            }
-            db.Gallery.updateOne(
-              { name: gallery.name, category: gallery.category },
-              {
-                $pull: { photos: { _id: mongodb.ObjectId(req.params.id) } },
-              },
-              (err, photo) => {
-                if (err) {
-                  console.log(err);
-                  return res.status(500).json({ err });
-                } else {
-                  res
-                    .status(200)
-                    .json({ message: "Photo successfully deleted" });
-                }
-              }
+        errCheck(err);
+        console.log(gallery);
+        if (gallery && location) {
+          try {
+            fs.unlinkSync(
+              `${__dirname}/../uploads/photos/${
+                gallery.category.toLowerCase() == "advertising"
+                  ? "Client-Work"
+                  : gallery.category.replace(/\/?\s+/g, "_")
+              }/${gallery.name.replace(/\/?\s+/g, "-")}/${location}`
             );
+            fs.unlinkSync(
+              `${__dirname}/../uploads/photos/${
+                gallery.category.toLowerCase() == "advertising"
+                  ? "Client-Work"
+                  : gallery.category.replace(/\/?\s+/g, "_")
+              }/${gallery.name
+                .replace(/\/?\s+/g, "_")
+                .replace(/[^\w\s]/gi, "")}/thumbs/${location}`
+            );
+          } catch (err) {
+            console.log(err);
           }
-
-          //
+          db.Gallery.updateOne(
+            { name: gallery.name, category: gallery.category },
+            {
+              $pull: { photos: { _id: mongodb.ObjectId(req.params.id) } },
+            },
+            (err, photo) => {
+              errCheck(err);
+              res.status(200).json({ message: "Photo successfully deleted" });
+            }
+          );
         }
       }
     );
@@ -121,68 +97,43 @@ module.exports = {
     const file = req.files.file;
     console.log(req.body);
 
-    file.mv(
-      `${__dirname}/../uploads/photos/${
-        req.body.category.toLowerCase() == "advertising"
-          ? "Client-Work"
-          : req.body.category.replace(/\/?\s+/g, "_")
-      }/${req.body.gallery.replace(/\/?\s+/g, "_").replace(/[^\w\s]/gi, "")}/${
-        file.name
-      }`,
-      (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send(err);
-        }
-        file.mv(
-          `${__dirname}/../uploads/photos/${
-            req.body.category.toLowerCase() == "advertising"
-              ? "Client-Work"
-              : req.body.category.replace(/\/?\s+/g, "_")
-          }/${req.body.gallery
-            .replace(/\/?\s+/g, "_")
-            .replace(/[^\w\s]/gi, "")}/thumbs/${file.name}`,
-          (err) => {
-            if (err) {
-              console.error(err);
-              return res.status(500).send(err);
-            }
+    let galleryPath = `${__dirname}/../uploads/photos/${
+      req.body.category.toLowerCase() == "advertising"
+        ? "Client-Work"
+        : req.body.category.replace(/\/?\s+/g, "_")
+    }/${req.body.gallery.replace(/\/?\s+/g, "_").replace(/[^\w\s]/gi, "")}`;
 
-            let newPhoto = req.body;
-            newPhoto.location = file.name;
-            newPhoto._id = mongodb.ObjectID();
-            console.log(newPhoto);
-            db.Gallery.updateOne(
+    file.mv(`${galleryPath}/${file.name}`, (err) => {
+      errCheck(err);
+      console.log("resizing image for thumbnail");
+      Jimp.read(`${galleryPath}/${file.name}`, (err, thumbnail) => {
+        errCheck(err);
+        thumbnail
+          .resize(480, Jimp.AUTO) // resize
+          .write(`${galleryPath}/thumbs/${file.name}`); // save
+
+        let newPhoto = req.body;
+        newPhoto.location = file.name;
+        newPhoto._id = mongodb.ObjectID();
+        console.log(newPhoto);
+        db.Gallery.updateOne(
+          { _id: mongodb.ObjectId(req.params.id) },
+          {
+            $push: { photos: newPhoto },
+          },
+          (err, photo) => {
+            errCheck(err);
+            db.Gallery.findOne(
               { _id: mongodb.ObjectId(req.params.id) },
-              {
-                $push: { photos: newPhoto },
-              },
-              (err, photo) => {
-                if (err) {
-                  console.log(err);
-                  return res.status(500).json({ err });
-                } else {
-                  db.Gallery.findOne(
-                    { _id: mongodb.ObjectId(req.params.id) },
-                    (err, gallery) => {
-                      if (err) {
-                        console.log(err);
-                        return res.status(500).json({ err });
-                      } else {
-                        res.status(200).json(gallery);
-                      }
-                    }
-                  );
-                  // res.status(200).res.json(newPhoto);
-                }
+              (err, gallery) => {
+                errCheck(err);
+                res.status(200).json(gallery);
               }
             );
-
-            //
           }
         );
-      }
-    );
+      });
+    });
   },
   copy: (req, res) => {
     let photo = req.body.photo;
@@ -191,36 +142,25 @@ module.exports = {
       db.Gallery.findOne(
         { "photos._id": mongodb.ObjectId(photo._id) },
         (err, gallery) => {
-          if (err) {
-            console.log(err);
-            return res.status(500).json({ err });
-          } else {
-            newPhoto = gallery.photos.find((item) => {
-              return photo._id == item._id;
-            });
-            newPhoto._id = mongodb.ObjectID();
-            newPhoto.order = 0;
-            db.Gallery.findOne(
-              { _id: mongodb.ObjectId(destinationGallery) },
-              (err, destGallery) => {
-                if (err) {
-                  console.log(err);
-                  return res.status(500).json({ err });
-                } else {
-                  newPhoto.category = destGallery.category;
-                  newPhoto.gallery = destGallery.name;
-                  copyFile(photo, newPhoto);
-                }
-              }
-            );
-          }
+          errCheck(err);
+          newPhoto = gallery.photos.find((item) => {
+            return photo._id == item._id;
+          });
+          newPhoto._id = mongodb.ObjectID();
+          newPhoto.order = 0;
+          db.Gallery.findOne(
+            { _id: mongodb.ObjectId(destinationGallery) },
+            (err, destGallery) => {
+              errCheck(err);
+              newPhoto.category = destGallery.category;
+              newPhoto.gallery = destGallery.name;
+              copyFile(photo, newPhoto);
+            }
+          );
         }
       );
     } catch (err) {
-      if (err) {
-        console.log(err);
-        return res.status(500).json({ err });
-      }
+      errCheck(err);
     }
 
     function copyFile(source, dest) {
@@ -252,12 +192,8 @@ module.exports = {
               `${sourcePath}/thumbs/${source.location}`,
               `${destPath}/thumbs/${dest.location}`,
               (err) => {
-                if (err) {
-                  console.log(err);
-                  return res.status(500).json({ err });
-                } else {
-                  addPhotoData(dest, destinationGallery);
-                }
+                errCheck(err);
+                addPhotoData(dest, destinationGallery);
               }
             );
           }
@@ -272,12 +208,8 @@ module.exports = {
           $push: { photos: photo },
         },
         (err, copiedPhoto) => {
-          if (err) {
-            console.log(err);
-            return res.status(500).json({ err });
-          } else {
-            res.json(copiedPhoto);
-          }
+          errCheck(err);
+          res.json(copiedPhoto);
         }
       );
     }
@@ -305,68 +237,58 @@ module.exports = {
             db.Gallery.findOne(
               { name: req.body.newPhoto.gallery },
               (err, gallery) => {
-                if (err) {
-                  console.log(err);
-                  return res.status(500).json({ err });
-                } else {
-                  if (req.body.reordered.photos) {
-                    // console.log(req.body.reordered);
-                    let photos = req.body.reordered.photos;
-                    console.log("updating photos");
-                    for (i = 0; i < photos.length; i++) {
-                      if (photos[i]._id && photos[i].order) {
-                        let final = i == photos.length - 1;
-                        db.Gallery.updateOne(
-                          {
-                            "photos._id": mongodb.ObjectId(photos[i]._id),
-                          },
+                errCheck(err);
+                if (req.body.reordered.photos) {
+                  // console.log(req.body.reordered);
+                  let photos = req.body.reordered.photos;
+                  console.log("updating photos");
+                  for (i = 0; i < photos.length; i++) {
+                    if (photos[i]._id && photos[i].order) {
+                      let final = i == photos.length - 1;
+                      db.Gallery.updateOne(
+                        {
+                          "photos._id": mongodb.ObjectId(photos[i]._id),
+                        },
 
-                          {
-                            $set: {
-                              "photos.$.order": photos[i].order,
-                            },
+                        {
+                          $set: {
+                            "photos.$.order": photos[i].order,
                           },
-                          (err, result) => {
-                            if (err) {
-                              console.log(err);
-                              return res.status(500).json(err);
-                            } else if (final) {
-                              console.log("getting updated gallery");
-                              db.Gallery.findOne(
-                                { name: req.body.newPhoto.gallery },
-                                (err, gallery) => {
-                                  if (err) {
-                                    console.log(err);
-                                    return res.status(500).json({ err });
-                                  } else {
-                                    console.log("sending response");
-                                    res.status(200).json(gallery);
-                                  }
+                        },
+                        (err, result) => {
+                          errCheck(err);
+                          if (final) {
+                            console.log("getting updated gallery");
+                            db.Gallery.findOne(
+                              { name: req.body.newPhoto.gallery },
+                              (err, gallery) => {
+                                if (err) {
+                                  console.log(err);
+                                  return res.status(500).json({ err });
+                                } else {
+                                  console.log("sending response");
+                                  res.status(200).json(gallery);
                                 }
-                              );
-                            }
+                              }
+                            );
                           }
-                        );
-                      } else if (i == photos.length - 1) {
-                        console.log("getting updated gallery");
-                        db.Gallery.findOne(
-                          { name: req.body.newPhoto.gallery },
-                          (err, gallery) => {
-                            if (err) {
-                              console.log(err);
-                              return res.status(500).json({ err });
-                            } else {
-                              console.log("sending response");
-                              res.status(200).json(gallery);
-                            }
-                          }
-                        );
-                      }
+                        }
+                      );
+                    } else if (i == photos.length - 1) {
+                      console.log("getting updated gallery");
+                      db.Gallery.findOne(
+                        { name: req.body.newPhoto.gallery },
+                        (err, gallery) => {
+                          errCheck(err);
+                          console.log("sending response");
+                          res.status(200).json(gallery);
+                        }
+                      );
                     }
-                  } else {
-                    console.log(gal);
-                    res.status(200).json(gallery);
                   }
+                } else {
+                  console.log(gal);
+                  res.status(200).json(gallery);
                 }
               }
             );
@@ -374,8 +296,7 @@ module.exports = {
         }
       );
     } catch (err) {
-      console.log(err);
-      res.status(500).json({ err });
+      errCheck(err);
     }
   },
 };
