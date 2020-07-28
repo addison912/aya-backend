ab0cf6e06105fc42d2cde5d2c4a63e614368083c
Addison Moore <addisondrewmoore@gmail.com>
2020-07-28 15:00:31 -0700

    updated start script to run indefinitely

--- start.js
+++ start.js
@@ -1,12 +1,17 @@
 const { exec } = require("child_process");
 
-exec("pm2 start index.js", (error, stdout, stderr) => {
-  if (error) {
-    console.log(`error: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.log(`stderr: ${stderr}`);
-    return;
-  }
-});
+function start() {
+  exec("pm2 start index.js", (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(stdout);
+  });
+}
+
+var restart = setInterval(() => start(), 86400000);
