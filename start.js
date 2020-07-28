bef4ae28c322e87a260b9e8dbeb382c7f60dedf4
Addison Moore <addisondrewmoore@gmail.com>
2020-07-28 14:43:55 -0700

    new start script with PM2

---
+++ start.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process");
+
+exec("pm2 start index.js", (error, stdout, stderr) => {
+  if (error) {
+    console.log(`error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+    return;
+  }
+});
